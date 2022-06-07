2c052b091fb5fb3b85923bc5e3171a0f0a987a29
maddnty <nzkb24@gmail.com>
2022-06-07 12:00:05 -0500

    created about me component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import React from 'react';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello World!</h1>
+        <main>
+          <About></About>
+        </main>
       </header>
     </div>
   );
