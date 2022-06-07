a493afdef6861b6cd20d6f49b3cb0942a6647d4d
maddnty <nzkb24@gmail.com>
2022-06-07 13:25:52 -0500

    hid dropdown

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,21 +1,6 @@
 import React from "react";
 import "bulma/css/bulma.min.css";
 
-const projects = [
-  {
-    name: "proj1",
-    description: "something cool about the project",
-  },
-  {
-    name: "proj2",
-    description: "something thats even cooler",
-  },
-  {
-    name: "proj3",
-    description: "the coolest thing ever",
-  },
-];
-
 function Nav() {
   return (
     <div>
@@ -29,8 +14,8 @@ function Nav() {
             About
           </a>
           <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">Projects</a>
-              <div class="navbar-dropdown">
+            <a class="navbar-link">Portfolio</a>
+              <div class="navbar-dropdown is-hidden">
                 <a class="navbar-item">Proj 1</a>
                 <a class="navbar-item">Proj 2</a>
                 <a class="navbar-item">Proj 3</a>
